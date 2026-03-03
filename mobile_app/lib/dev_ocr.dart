bca0ffc3ed0fdafec1094ed90d7fec3bb246574e
Sarah <444200758@student.ksu.edu.sa>
2026-03-03 03:48:49 +0300

    add dev ocn

---
+++ mobile_app/lib/dev_ocr.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+import 'screens/ocr_page.dart';
+
+void main() {
+  runApp(
+    const MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: OcrPage(),
+    ),
+  );
+}
