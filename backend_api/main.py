2fb433c432feb87017ff243c85ce691dcefddd72
Sarah <444200758@student.ksu.edu.sa>
2026-03-07 21:42:27 +0300

    add ocr backend file

--- backend_api/main.py
+++ backend_api/main.py
@@ -1,6 +1,12 @@
 from fastapi import FastAPI
+# Import routers from different route files
+from routes.ocr import router as ocr_router
 
+# Create FastAPI
 app = FastAPI()
+
+# Include routers
+app.include_router(ocr_router, prefix="/ocr", tags=["OCR"])
 
 @app.get("/")
 def root():
